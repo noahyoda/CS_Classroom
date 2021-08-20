6f7933728d78d2d29e467f3803ec98c25e3b79cb
BuildTools <noahyoda1@gmail.com>
2021-08-19 21:55:24 -0600

    finished rough add student logic draft

--- Program.cs
+++ Program.cs
@@ -7,10 +7,34 @@ namespace Classroom
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my Classroom project!");
+            ArrayList<Student> classroom = new ArrayList<Student>;
             while(true){
                 Console.WriteLine("What would you like to do?");
                 Console.WriteLine("1 - Add a student\n2 - Remove a student\n3 - List all students\n4 - Export students to file\n5 - Exit");
                 int choice = Console.Read();
+
+                switch(choice){
+                    case == 1:
+                        Console.WriteLine("What is the students name:\n");
+                        String name = Console.ReadLine();
+                        Console.WriteLine("What is the students grade:\n");
+                        int grade = Console.ReadLine();
+                        Console.WriteLine("And what is the students gpa:\n");
+                        double gpa = Console.ReadLine();
+                        Student newStudent = Student(name, grade, gpa);
+                        classroom.Add(newStudent);
+                        Console.WriteLine("Student added successfully");
+                        break;
+                    case == 2:
+                        break;
+                    case == 3:
+                        break;
+                    case == 4:
+                        break;
+                    case == 5:
+                        break;
+                }
+
             }
         }
     }
